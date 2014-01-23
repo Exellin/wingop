d34a327c3b479bac307f5c848b3e683adeeda068
Exellin <carnegieshawn@yahoo.ca>
2014-01-23 20:42:13 +0000

    functional readcoefficients.  Values hardcoded for now

--- readcoefficients/readcoefficients.c
+++ readcoefficients/readcoefficients.c
@@ -5,25 +5,25 @@
 
 int main ()
 {
+float precision = 0.0001;
 int i = 0;
-float aoa; 					//angle of attack
+float aoa = 1; 				//angle of attack
 float aoatest;
-float Re;  					//Reynolds number
+float Re = 10000;  			//Reynolds number
 char file_name[25] = ""; 	//initializing filename with an empty string
-char blanktest = ' ';					//testing for blanks when reading file
+char blanktest = ' ';		//testing for blanks when reading file
 char Restring[25];			//variable that will hold Reynolds number as a string
 char Restringtrunc[25]; 	//Reynolds number rounded to nearest 5000
-char aoastring[25];			//angle of attack string
+char aoastring[25] = "";	//angle of attack string
 int Reincrement = 5000; 
 char line[100];				//string to hold line of file
 double Cl;
 double Cd;
-char Clstring[25];
-char Cdstring[25];
+char Clstring[25] = "";
+char Cdstring[25] = "";
 int KeepLooping = 1;
 FILE *filepointer;
 
-scanf("%f %g", &aoa, &Re);
 strcat(file_name, "./s1223rawdata/S1223_T1_Re"); 			//1st part of filename
 Re = floor(Re/Reincrement)*Reincrement; 					//rounding to nearest 5000
 sprintf(Restring, "%f", Re/1000000); 						//converting to string
@@ -31,8 +31,7 @@ strncpy(Restringtrunc, Restring, 5);						//truncating to 5 digits
 Restringtrunc[5] = '\0'; 									//adding 0 to make it a string
 strcat(file_name, Restringtrunc); 							//adding 2nd part of filename
 strcat(file_name, "_M0.00_N9.0.txt "); 						//adding last part of filename
-printf("%s", file_name);	
-
+printf("%s\n", file_name);	
 
 filepointer = fopen(file_name, "r"); //read mode
 if (filepointer == NULL)
@@ -43,60 +42,70 @@ if (filepointer == NULL)
 
 while(1 && KeepLooping)			
 {
-	i = 0;
-	fgets(line, 100, filepointer);
-	
-	while (blanktest == ' ') //read AoA
+	while (fgets(line, 100, filepointer) && KeepLooping)
 	{
-		blanktest = line[i];
-		i++;
+		memset(aoastring, '\0', strlen(aoastring));
+		i = 0;
+		//printf("fgets contains %s", line);
+		while (blanktest == ' ') //read AoA
+		{
+			blanktest = line[i];
+			i++;
+		}
+		while (blanktest != ' ')
+		{
+			aoastring[strlen(aoastring)] = blanktest;
+			//printf("string length %d containing %s\n", strlen(aoastring), aoastring);
+			blanktest=line[i];
+			i++;
+		}
+			sscanf(aoastring, "%f", &aoatest);
+			//printf("aoa is %g, aoatest is %g\n", aoa, aoatest);
+		
+		if (((aoa - precision) < aoatest) && ((aoa+precision) > aoatest) && aoastring[strlen(aoastring)-1] == '0') //testing equality and last character is 0 to guarantee it is data
+		{
+			//printf("aoatest equals aoa\n");
+			KeepLooping = 0;
+			
+			while (blanktest == ' ') //read CL
+			{
+				blanktest = line[i];
+				i++;
+			}	
+			while (blanktest != ' ')
+			{
+				Clstring[strlen(Clstring)] = blanktest;
+				blanktest=line[i];
+				i++;
+			}
+			sscanf(Clstring, "%lf", &Cl);
+		
+			while (blanktest == ' ') //read Cd
+			{
+				blanktest = line[i];
+				i++;
+			}
+			while (blanktest != ' ')
+			{
+				Cdstring[strlen(Cdstring)] = blanktest;
+				blanktest=line[i];
+				i++;
+			}
+			sscanf(Cdstring, "%lf", &Cd);
+			printf("At an aoa of %f and a Reynolds number of %f, the Cl is %g and Cd is %g", aoa, Re, Cl, Cd);
+		}
 	}
-	while (blanktest != ' ')
-	{
-		aoastring[strlen(aoastring)] = blanktest;
-		//strcat(aoastring, blanktest);
-		blanktest=line[i];
-		i++;
-	}
-		sscanf(aoastring, "%f", &aoatest);
-	
-	if (aoa == aoatest)
+	if (feof(filepointer)) //with 3 blank lines at end of file, aoastring is 25.000 3 times until the file ends.
 	{
 		KeepLooping = 0;
-		
-		while (blanktest == ' ') //read CL
-		{
-			blanktest = line[i];
-			i++;
-		}	
-		while (blanktest != ' ')
-		{
-			Clstring[strlen(Clstring)] = blanktest;
-			//strcat(Clstring, blanktest);
-			blanktest=line[i];
-		}
-		sscanf(Clstring, "%lf", &Cl);
-	
-		while (blanktest == ' ') //read Cd
-		{
-			blanktest = line[i];
-			i++;
-		}
-		while (blanktest != ' ')
-		{
-		Cdstring[strlen(Cdstring)] = blanktest;
-		//strcat(Cdstring, blanktest);
-		blanktest=line[i];
-		}
-		sscanf(Cdstring, "%lf", &Cd);
 	}
 }
-//until " " character is not met
-//read characters until " " character is met
-//if string size is above 1, to avoid line 5 in file
-//store in variable
-//convert to double
-//if double is equal to AoA, read other coefficients
-//if not, go to next line
 return 0;
 }
+// //until " " character is not met
+// //read characters until " " character is met
+// //if string size is above 1, to avoid line 5 in file
+// //store in variable
+// //convert to double
+// //if double is equal to AoA, read other coefficients
+// //if not, go to next line
