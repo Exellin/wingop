34a178252d7bad57e10094892112168fe1e29469
Exellin <carnegieshawn@yahoo.ca>
2014-02-02 20:28:31 +0000

    momsim kind of working.  Latest test runs out of runway.

--- momsim/momsim.c
+++ momsim/momsim.c
@@ -17,6 +17,7 @@ double integ = 0; //Current integral state for PID controller
 
 void defleccoefficients(double Re, double alpha, double deflec, double* Cldeflecpointer, double* Cddeflecpointer, double* Cmdeflecpointer);
 double pid(double, double, double, double, double, double); //Forward declaration
+double findmaxalpha(double Re, double alpha, double deflec);
 
 double linthrust(double v) //Thrust output
 {
@@ -36,6 +37,106 @@ double linthrust(double v) //Thrust output
 	{
 		return t[(int) floor(v)] + (t[(int) ceil(v)] - t[(int) floor(v)])*((v - floor(v))/(ceil(v) - floor(v)));
 	}
+}
+
+double findmaxalpha(double Re, double alpha, double deflec)  //only finds max angle of attack at the rounded values.
+{
+int i = 0;
+double Cltest = 0;		
+double Clprevious = 0;	// to guarantee the first point won't have a lower Cl since it is negative
+double alphatest = 0;
+char file_name[25] = ""; 	//initializing filename with an empty string
+char blanktest = ' ';		//testing for blanks when reading file
+char Restring[25];			//variable that will hold Reynolds number as a string
+char Restringtrunc[25]; 	//Reynolds number rounded to nearest 5000
+char alphastring[25] = "";	//angle of attack string
+char deflecstring[25] = "";
+int Reincrement = 5000; 
+char line[100];				//string to hold line of file
+int KeepLooping = 1;
+char Clstring[25] = "";
+FILE *filepointer;
+
+strcat(file_name, "./s1223rawdata/"); 						//1st part of filename
+Re = floor(Re/Reincrement)*Reincrement; 					//rounding to nearest 5000, to a minimum of 5000
+if (Re < 5000)
+{
+Re = 5000;
+}
+alpha = round(alpha);										//rounding to nearest integer
+deflec = floor(deflec/5)*deflec;							//rounding to nearest 5
+sprintf(deflecstring, "%d", (int) deflec);
+sprintf(Restring, "%lf", Re/1000000); 						//converting to string
+strncpy(Restringtrunc, Restring, 5);						//truncating to 5 digits
+Restringtrunc[5] = '\0'; 									//adding 0 to make it a string
+strcat(file_name, deflecstring);
+strcat(file_name, "s1223_T0_Re");
+strcat(file_name, Restringtrunc); 							
+strcat(file_name, "_M0.00_N9.0.txt "); 						
+printf("%s\n", file_name);	
+
+filepointer = fopen(file_name, "r"); //read mode
+if (filepointer == NULL)
+{
+	perror("Error while opening the file.\n");
+	exit(EXIT_FAILURE);
+}
+
+while(1 && KeepLooping)			
+{
+	while (fgets(line, 100, filepointer) && KeepLooping)
+	{
+		memset(alphastring, '\0', strlen(alphastring));
+		memset(Clstring, '\0', strlen(Clstring));
+		i = 0;
+		//printf("fgets contains %s", line);
+		while (blanktest == ' ') //read AoA
+		{
+			blanktest = line[i];
+			i++;
+		}
+		while (blanktest != ' ')
+		{
+			alphastring[strlen(alphastring)] = blanktest;
+			blanktest=line[i];
+			i++;
+		}
+		sscanf(alphastring, "%lf", &alphatest);
+
+		while (blanktest == ' ') //read CL
+		{
+			blanktest = line[i];
+			i++;
+		}	
+		while (blanktest != ' ')
+		{
+			Clstring[strlen(Clstring)] = blanktest;
+			//printf("Clstring is %s\n", Clstring);
+			blanktest=line[i];
+			i++;
+		}
+		if (alphastring[strlen(alphastring)-1] == '0') //guarantee it is data
+		{
+
+			Clprevious = Cltest;
+			sscanf(Clstring, "%lf", &Cltest);
+			//printf("at an aoa of %lf, the Cl is %lf and Clprevious is %lf\n", alphatest, Cltest, Clprevious);
+			if (Cltest < Clprevious && Cltest >0) //testing if Cl has decreased after going positive
+			{
+				KeepLooping = 0;
+				fclose(filepointer);
+				return alphatest;
+			}
+		}
+	}
+	if (feof(filepointer)) //with 3 blank lines at end of file, alphastring is 25.000 3 times until the file ends.
+	{
+		KeepLooping = 0;
+	}
+}
+fclose(filepointer);
+printf("There was no maximum Cl\n");
+return alphatest;
 }
 
 void updatecoefficients(double Re, double alpha, double deflec, double* Clpointer, double* Cdpointer, double* Cmpointer)
@@ -118,6 +219,7 @@ while(1 && KeepLooping)
 				i++;
 			}
 			sscanf(Clstring, "%lf", Clpointer);
+			//printf("at an aoa of %lf, the Cl is %lf\n", alphatest, *Clpointer);
 
 			while (blanktest == ' ') //read Cd
 			{
@@ -282,60 +384,63 @@ int main(int argc, char** argv) //argc is argument count, argv is an array of sp
 		
 		printf("At an aoa of %g and a Reynolds number of %g and %d degrees of deflection, the Cl is %lf, Cd is %lf, and Cm is %lf\n", alpha, Re, 0, Cl, Cd, Cm);
 		printf("At an aoa of %g and a Reynolds number of %g and %g degrees of deflection, the Cl is %lf, Cd is %lf, and Cm is %lf\n", alpha, Re, deflec, Cldeflec, Cddeflec, Cmdeflec);
-		// double fclift = .5*RHO*pow(vn, 2)*Cl*B*C; //Calculating lift, front wing
-		// puts("1");
-		// double fcdrag = .5*RHO*pow(vn, 2)*Cd*B*C;
-		// puts("2");
-		// double rclift = .125*RHO*pow(vn, 2)*Cldeflec*B*C + .375*RHO*pow(vn, 2)*Cl*B*C; //Back wing with aileron covering 1/4 of the span
-		// puts("3");
-		// double rcdrag = .125*RHO*pow(vn, 2)*Cddeflec*B*C + .375*RHO*pow(vn, 2)*Cd*B*C;
-		// puts("4");
-		// if((2*fclift + 2*rclift) > M*G) //See if it takes off
-		// {
-			// printf("Takeoff!\n");
-			// break;
-		// }
-		// vn1 = ((linthrust(vn) - (2*fcdrag+2*rcdrag))/M)*ts + vn; //Numerical integral for speed
-		// puts("5");
-		// distn = distn + .5*ts*(vn1 + vn); //Numerical integral for distance
-		// time += ts;
-		// if(distn > maxdist) //If we exceed runway length, fail
-		// {
-			// printf("No takeoff.\n");
-			// break;
-		// }
-		// deflec = pid(2, 1, 0, maxa, alpha, ts); //Put the current state into the PID controller and set the flap deflection
-		// puts("6");
-		// double fcmom = 2*C*C*B*Cm + 2*fclift*D*.5; //Moment at the front wing
-		// double rcmom = 0.25*2*C*C*B*Cmdeflec + 0.75*2*C*C*B*Cm + -2*rclift*D*.5; //Moment at the rear wing
-		// puts("7");
+		double fclift = .5*RHO*pow(vn, 2)*Cl*B*C; //Calculating lift, front wing
+		puts("1");
+		double fcdrag = .5*RHO*pow(vn, 2)*Cd*B*C;
+		puts("2");
+		double rclift = .125*RHO*pow(vn, 2)*Cldeflec*B*C + .375*RHO*pow(vn, 2)*Cl*B*C; //Back wing with aileron covering 1/4 of the span
+		puts("3");
+		double rcdrag = .125*RHO*pow(vn, 2)*Cddeflec*B*C + .375*RHO*pow(vn, 2)*Cd*B*C;
+		puts("4");
+		if((2*fclift + 2*rclift) > M*G) //See if it takes off
+		{
+			printf("Takeoff!\n");
+			break;
+		}
+		vn1 = ((linthrust(vn) - (2*fcdrag+2*rcdrag))/M)*ts + vn; //Numerical integral for speed
+		puts("5");
+		distn = distn + .5*ts*(vn1 + vn); //Numerical integral for distance
+		time += ts;
+		if(distn > maxdist) //If we exceed runway length, fail
+		{
+			printf("No takeoff.\n");
+			break;
+		}
+		maxa = findmaxalpha(Re, alpha, 5);
+		printf("plane reaches stall at %lf\n", maxa);
+		deflec = pid(.01, .01, 0, maxa, alpha, ts); //Put the current state into the PID controller and set the flap deflection
+		printf("new deflection is %lf, moving at a rate of %lf degrees per second\n", deflec, deflec/ts);
+		puts("6");
+		double fcmom = 2*C*C*B*Cm + 2*fclift*D*.5; //Moment at the front wing
+		double rcmom = 0.25*2*C*C*B*Cmdeflec + 0.75*2*C*C*B*Cm + -2*rclift*D*.5; //Moment at the rear wing
+		puts("7");
 		// printf("front wing moment coefficient: %g\nrear wing moment coefficient: %g\n", Cm, 0.25*Cmdeflec+0.75*Cm);
-		// printf("Front Moment: %g\nRear Moment: %g\n", fcmom, rcmom);
-		// angv1 = (fcmom + rcmom)/((1/3)*(pow(Z,2)+pow((D+2*C),2)))*ts+angv; //Angular numerical velocity integral, Replace 1 with M
-		// printf("angv: %g\nangv1 %g\n", angv, angv1);
-		// alpha = (alpha + (180/M_PI)*0.5*ts*(angv + angv1)); //Numerical angular position integral
-		// printf("%gs: Lift: %g Drag: %g Thrust: %g Speed: %g Distance: %g Alpha: %g Deflection:%g Moment: %g\n", time, (2*fclift + 2*rclift), (2*fcdrag + 2*rcdrag), linthrust(vn1), vn1, distn, alpha, deflec, (fcmom+rcmom));
-		break;
+		printf("Front Moment: %g\nRear Moment: %g\n", fcmom, rcmom);
+		printf("%lf divided by %lf is %lf\n", (fcmom + rcmom)*ts, (M/3)*(pow(Z,2)+pow((D+2*C),2)), (fcmom + rcmom)/((M/3)*(pow(Z,2)+pow((D+2*C),2)))*ts);
+		angv1 = (fcmom + rcmom)/((M/3)*(pow(Z,2)+pow((D+2*C),2)))*ts+angv; //Angular numerical velocity integral
+		alpha = (alpha + (180/M_PI)*0.5*ts*(angv + angv1)); //Numerical angular position integral
+		printf("the angle of attack is %lf, with the plane rotating at %lf radians per second\n", alpha, angv1);
+		printf("%gs: Lift: %g Drag: %g Thrust: %g Speed: %g Distance: %g Alpha: %g Deflection:%g Moment: %g\n", time, (2*fclift + 2*rclift), (2*fcdrag + 2*rcdrag), linthrust(vn1), vn1, distn, alpha, deflec, (fcmom+rcmom));
 	}
 	return 0;
 }
 
-// double pid(double kp, double ki, double kd, double sp, double mv, double ts) //PID controller for the plane
-// {
-	// double err = sp - mv; //Error from set point and measured value
-	// integ = integ + err*ts; //Integral (numerical)
-	// double deriv = (err - preverr)/ts; //Derivative (numerical)
-	// preverr = err; 
-	// if((kp*err + ki*integ + kd*deriv) >= MAXF)
-	// {
-		// return MAXF; //Maximum deflection
-	// }
-	// else if((kp*err + ki*integ + kd*deriv) <=-MAXF)
-	// {
-		// return -MAXF;
-	// }
-	// else
-	// {
-		// return (kp*err + ki*integ + kd*deriv); //Return the PID
-	// }
-// }
+double pid(double kp, double ki, double kd, double sp, double mv, double ts) //PID controller for the plane
+{
+	double err = sp - mv; //Error from set point and measured value
+	integ = integ + err*ts; //Integral (numerical)
+	double deriv = (err - preverr)/ts; //Derivative (numerical)
+	preverr = err; 
+	if((kp*err + ki*integ + kd*deriv) >= MAXF)
+	{
+		return MAXF; //Maximum deflection
+	}
+	else if((kp*err + ki*integ + kd*deriv) <=-MAXF)
+	{
+		return -MAXF;
+	}
+	else
+	{
+		return (kp*err + ki*integ + kd*deriv); //Return the PID
+	}
+}
